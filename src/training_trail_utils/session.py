133d6d97e4dca5a93cd9e592f2d5d9abf26f61d3
PhilFiftyEight <123263626+PhilFiftyEight@users.noreply.github.com>
2023-02-11 12:20:32 +0100

    📦 NEW: Session class

--- src/training_trail_utils/session.py
+++ src/training_trail_utils/session.py
@@ -1,2 +1,20 @@
-class Session():
-    pass
+from dataclasses import dataclass, field
+
+import pendulum
+
+def defaultduration():
+    return pendulum.duration()
+
+def defaultdate():
+    return pendulum.datetime(1900,1,1)
+
+@dataclass
+class Session:
+    datas: str
+    date: pendulum.DateTime = field(default_factory=defaultdate)
+    duration: pendulum.Duration = field(default_factory=defaultduration)
+    vma: pendulum.Duration = field(default_factory=defaultduration)
+    anaerobie: pendulum.Duration = field(default_factory=defaultduration)
+    end_active: pendulum.Duration = field(default_factory=defaultduration)
+    end_fond: pendulum.Duration = field(default_factory=defaultduration)
+    recup: pendulum.Duration = field(default_factory=defaultduration)
