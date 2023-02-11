2f16310fe1a94b793fd26c8729fda5a3d32264bd
PhilFiftyEight <123263626+PhilFiftyEight@users.noreply.github.com>
2023-02-11 16:44:25 +0100

    👌 IMPROVE: Refactor post_init, format with black

--- src/training_trail_utils/session.py
+++ src/training_trail_utils/session.py
@@ -2,11 +2,14 @@ from dataclasses import dataclass, field
 
 import pendulum
 
+
 def defaultduration():
     return pendulum.duration()
 
+
 def defaultdate():
-    return pendulum.datetime(1900,1,1)
+    return pendulum.datetime(1900, 1, 1)
+
 
 @dataclass
 class Session:
@@ -21,55 +24,67 @@ class Session:
 
     def __post_init__(self):
         # example of self.datas : "2022-2-10 51:54 08:46 27:16 03:31 06:31 05:45"
-        datasList = self.datas.split(' ')
-        
-        date = [ int(data) for data in datasList.pop(0).split('-')]
+        def extract_datas():
+            datasList = self.datas.split(" ")
+            for datas in datasList:
+                yield [
+                    int(data)
+                    for data in (datas.split(":") if ":" in datas else datas.split("-"))
+                ]
+
+        datas = extract_datas()
+
+        date = next(datas)
         self.date = pendulum.local(*date)
-        
-        duration = [ int(data) for data in datasList.pop(0).split(':')]
+
+        duration = next(datas)
         try:
             hours, minutes, seconds = duration
         except ValueError:
             minutes, seconds = duration
             hours = 0
-        self.duration = pendulum.duration(seconds=seconds, minutes=minutes, hours=hours )
-        
-        vma = [ int(data) for data in datasList.pop(0).split(':')]
+        self.duration = pendulum.duration(seconds=seconds, minutes=minutes, hours=hours)
+
+        vma = next(datas)
         try:
             hours, minutes, seconds = vma
         except ValueError:
             minutes, seconds = vma
             hours = 0
-        self.vma = pendulum.duration(seconds=seconds, minutes=minutes, hours=hours )
+        self.vma = pendulum.duration(seconds=seconds, minutes=minutes, hours=hours)
 
-        anaerobie = [ int(data) for data in datasList.pop(0).split(':')]
+        anaerobie = next(datas)
         try:
             hours, minutes, seconds = anaerobie
         except ValueError:
             minutes, seconds = anaerobie
             hours = 0
-        self.anaerobie = pendulum.duration(seconds=seconds, minutes=minutes, hours=hours )
-        
-        end_active = [ int(data) for data in datasList.pop(0).split(':')]
+        self.anaerobie = pendulum.duration(
+            seconds=seconds, minutes=minutes, hours=hours
+        )
+
+        end_active = next(datas)
         try:
             hours, minutes, seconds = end_active
         except ValueError:
             minutes, seconds = end_active
             hours = 0
-        self.end_active = pendulum.duration(seconds=seconds, minutes=minutes, hours=hours )
+        self.end_active = pendulum.duration(
+            seconds=seconds, minutes=minutes, hours=hours
+        )
 
-        end_fond = [ int(data) for data in datasList.pop(0).split(':')]
+        end_fond = next(datas)
         try:
             hours, minutes, seconds = end_fond
         except ValueError:
             minutes, seconds = end_fond
             hours = 0
-        self.end_fond = pendulum.duration(seconds=seconds, minutes=minutes, hours=hours )
+        self.end_fond = pendulum.duration(seconds=seconds, minutes=minutes, hours=hours)
 
-        recup = [ int(data) for data in datasList.pop(0).split(':')]
+        recup = next(datas)
         try:
             hours, minutes, seconds = recup
         except ValueError:
             minutes, seconds = recup
             hours = 0
-        self.recup = pendulum.duration(seconds=seconds, minutes=minutes, hours=hours )
+        self.recup = pendulum.duration(seconds=seconds, minutes=minutes, hours=hours)
